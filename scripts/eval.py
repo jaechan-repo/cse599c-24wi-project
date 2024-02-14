1213fe1e4769b869fcdfaa5416a0f883d708a81e
Aimon Benfield-Chand <aimonbc@gmail.com>
2024-02-13 21:11:30 -0800

    update eval.py for compatibility with Aligner

--- scripts/eval.py
+++ scripts/eval.py
@@ -1,5 +1,6 @@
 ### Evaluation code for audio-score alignment
 
+import json
 import torch
 from torch.utils.data import DataLoader
 import tqdm
@@ -11,22 +12,25 @@ sys.path.append('..')
 from aligner.utils.metrics import temporal_distance_vec, binary_accuracy_vec, monotonicity_vec, score_coverage_vec
 from aligner.utils.metrics import compute_loss
 from aligner.utils.decode import max_decode, DTW
-from aligner.dataset import MaestroDataset
 
-# TODO: Update the following imports with actual model
-from aligner.model import Aligner
+from aligner.dataset import MaestroDataset
+from aligner.utils.constants import *
+from aligner.model import AlignerModel, ModelConfig
+from aligner.utils.constants import *
+
 
 # TODO: Add imports for other evaluation benchmark datasets
 
 
 def load_model(model_path):
-    model = Aligner()   # TODO: Update with actual model + args
+    model = AlignerModel(ModelConfig())
     model.load_state_dict(torch.load(model_path))
     model.eval()
     return model
 
+
 def evaluate(
-        model: torch.nn.Module, 
+        model, 
         dataloader: torch.utils.data.DataLoader, 
         decoding: Callable[[torch.Tensor], torch.Tensor], 
         tolerance: float) -> Tuple[float, float, float, float, float]:
@@ -34,7 +38,7 @@ def evaluate(
     """Evaluate the model on the evaluation dataset.
     
     Args:
-        model (torch.nn.Module): Model to evaluate.
+        model: Model to evaluate.
         dataloader (torch.utils.data.DataLoader): Evaluation dataset.
         decoding (Callable): Decoding method to use on cross-attention alignment matrix. Either \textbf{max_decode} or \textbf{DTW}.
         tolerance (float): Tolerance threshold for alignment distance.
@@ -54,31 +58,17 @@ def evaluate(
 
     # Compute loss and metrics for each batch with tqdm
     for batch in tqdm(dataloader):
-        '''
-        Each sample is a dictionary with the following fields:
-            (0) "wav_uri": absolute path to audio file
-            (1) "midi_uri": absolute path to midi file
-            (2) "audio_clip": ~10 sec spectrogram. Size: (n_mels, n_frames).
-            (3) "Y": alignment matrix. Size: (n_frames, n_events).
-            (4) "input_ids": Integer array representation (tokenization) of the MIDI.
-                This is the encoding that the transformer model receives. During
-                tokenization, MIDI-scaled pitches [0, 121] are directly taken as unique 
-                tokens, and special tokens such as [BOS] and [event] are attached. See
-                `TOKEN_ID` in `constants.py` for their integer codes. Size: (n_tokens,).
-            (5) "event_mask": Array filled with 0s and 1s, where 1s fill positions where
-                event markers lie in "input_ids". Size: (n_tokens,).
-            (6) "attn_mask": mask for self-attention. Size: (n_tokens, n_tokens).'''
-        
-        # Assuming batch of shape (N, E, X), where N is the batch size, E is the number of MIDI events in the score, and X is the number of audio frames
-        # TODO: Update with actual batch format
-        audio_frames, score_events, Y, midi_event_timestamps = batch
+        Y = batch.Y_b.transpose(-2, -1) # (N, X, E) -> (N, E, X)
 
         # get model predictions given audio frames and score events
-        # TODO: Update with actual model forward pass API
-        Y_pred = model(audio_frames, score_events)
+        Y_pred = model(
+            batch.audio_frames_b,
+            score_ids=batch.score_ids_b,
+            score_attn_mask=batch.score_attn_mask_b,
+            event_padding_mask=batch.event_padding_mask_b
+        ).tranpose(-2, -1) # (N, X, E) -> (N, E, X)
 
-        # loss should use the non-decoded alignment matrix???
-        total_loss += compute_loss(Y_pred, Y, midi_event_timestamps, 'mean')
+        total_loss += compute_loss(Y_pred, batch.Y, midi_event_timestamps, 'mean')
 
         # decode soft cross-attn alignment matrix into binary alignment matrix
         Y_pred_binary = decoding(Y_pred)
@@ -107,10 +97,30 @@ def main():
 
     model = load_model(model_path)
     evaluation_dataset = MaestroDataset(evaluation_data_path, 'test')
-    dataloader = DataLoader(evaluation_dataset, batch_size=32)
+    dataloader = DataLoader(
+        dataset=evaluation_dataset, 
+        num_workers=4,
+        collate_fn=MaestroDataset.collate_fn
+    )
     
     loss, distance, accuracy, monotonicity, coverage = evaluate(model, dataloader, decoding, tolerance)
     print(f'Evaluation Metrics with Tau={tolerance}\nLoss: {loss:.2f}, Distance: {distance:.2f}, Accuracy: {accuracy:.2f}, Monotonicity: {monotonicity:.2f}, Coverage: {coverage:.2f}')
+
+    # organize metrics into a dictionary
+    metrics = {
+        'Loss': loss,
+        'Distance': distance,
+        'Accuracy': accuracy,
+        'Monotonicity': monotonicity,
+        'Coverage': coverage
+    }
+
+    metrics_json = json.dumps(metrics)
+    
+    # write the JSON string to a file
+    with open('metrics.json', 'w') as f:
+        f.write(metrics_json)
+
 
 if __name__ == "__main__":
     main()
